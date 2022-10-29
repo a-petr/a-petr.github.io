e38e348a3e6f492414451213b068314f3b004569
Petrosyan <apetrosyan3@lawn-128-61-121-119.lawn.gatech.edu>
2022-10-29 16:44:51 -0400

    small

--- gitpush.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# lines that start like this are shell comments
-# read projects current directory with $PWD
-echo “running command from” $PWD
-cd $PWD
-git add -A
-echo “Enter commit message: “
-git commit -m “$commitMessage”
-git push origin gh-pages
